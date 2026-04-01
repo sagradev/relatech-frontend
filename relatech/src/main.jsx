70b425e10e893afc295ec7a224d91edb5cb5ec45
Gustavo Rodrigues Gutierrez <mmpguty.5@gmail.com>
2026-04-01 16:37:26 -0400

    ajustado main.jsx para tentar ajustar bug de login

--- relatech/src/main.jsx
+++ relatech/src/main.jsx
@@ -6,20 +6,22 @@ import LoginPage from './LoginPage.jsx'
 function Root() {
   const [auth, setAuth] = useState(() => {
     const token = localStorage.getItem("relatech:token");
+    console.log(">>> Token no Root:", token);
+    console.log(">>> Auth:", !!token);
     return !!token;
   });
 
+  console.log(">>> Renderizando Root, auth =", auth);
+
   if (!auth) {
+    console.log(">>> Mostrando LoginPage");
     return <LoginPage onLogin={() => setAuth(true)} />;
   }
 
+  console.log(">>> Mostrando App");
   return <App onLogout={() => {
     localStorage.removeItem("relatech:token");
     localStorage.removeItem("relatech:user");
     setAuth(false);
   }} />;
 }
-
-createRoot(document.getElementById('root')).render(
-  <Root />
-)
